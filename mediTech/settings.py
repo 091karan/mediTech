6522c8dcddf27812d37e022db616ac994362eb98
Karan Agarwal <karan@Karans-MacBook-Pro.local>
2018-12-27 23:33:32 +0530

    Initial migration

--- mediTech/settings.py
+++ mediTech/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mediTech.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'meditechdb',
+        'USER': 'postgres',
+        'PASSWORD': 'karan091',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
